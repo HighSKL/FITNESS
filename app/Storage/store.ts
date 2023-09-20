2c11f455471dc96926839e3cc34ec548ceeef37a
HighSKL <kostirko02@gmail.com>
2023-09-20 23:26:45 +0700

    profile->home&bugfix

--- app/Storage/store.ts
+++ app/Storage/store.ts
@@ -1,12 +1,7 @@
 import { UserDataType } from "../types";
 
-// export const data = {
-//     userData: null as UserDataType|null
-// }
-
-
 export const data = {
-    userData: {email: "a@a.a", height: null, id: 17, iswellcomebriefingcomplete: false, userimg: null, username: "Алексей", weight: null} as any|null
+    userData: null as UserDataType|null
 }
 
 export const setUserData = (newUserData: UserDataType) => { data.userData = newUserData }
